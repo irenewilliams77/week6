8dc6c2b0f060b2ba7eff68ff70df6961b0bbf859
Irene Williams <europeandistribution@ymail.com>
2020-08-02 10:37:46 -0400

    created promis

---
+++ promises.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("timeDelay");
+
+button.addEventListener("click", function() {
+   msgTimeout = setTimeout(function() {
+        alert("whats up?");
+    }, 3000);
+    });
+
+    Promise.resolve().then(r =>console.log("resolved"));
+
+    const cancelButton = document.getElementById("cancelTimeDelay");
+    let msgTimeout = null;
+    
+    cancelButton.addEventListener("click", function(){
+        console.log(msgTimeout);
+        clearTimeout(msgTimeout);
+    })
