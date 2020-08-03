d1b90e14c902c1851e0f77a6372609de2cbc961c
Irene Williams <europeandistribution@ymail.com>
2020-08-03 13:30:27 -0400

    i tried

--- editting.js
+++ editting.js
@@ -16,11 +16,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', addFull);
 });*/
 
-var fs = require('fs');
+/*var fs = require('fs');
 
 fs.readFile( 'mainuser.txt','utf8', function(err, data){
     if(err) throw err;
 console.log(data);
+});*/
+
+/*false.watch('mainuser'), (click, mainuser.text) => {
+    console.log('event type is: ${eventType');
+    if(mainuser){
+        console.log('mainuser:${mainuser.txt');
+
+    }else {
+        console.log('mainuser not provided');
+    }
+};
+
+false.watch('mainuser', (curr, prev)=>{
+    console.log ('the current mtime is:$(curr,mtime}');
+    console.log('the previous mtime: ${prev.mtime}');
+});*/
+
+var follow = require('text-file-follower');
+
+var follower = follow('week6_mainuser.txt')
+
+follower.on('line', function(filename, line){
+    console.log(line);
 });
 
-
+console.log('line added');
